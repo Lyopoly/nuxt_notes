514f7551f4de1ed1537f0703370dfc304d433399
Lyopoly <lydellkid@gmail.com>
2026-07-22 10:56:41 +0800

    iterate3

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <UPageHero
-      title="LyoNuxt Notes"
+      title="LyoNuxt Notes Cloudflare test"
       description="A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours."
       :links="[{
         label: 'Get started',
