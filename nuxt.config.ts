ecb143e27a9ff28c3aa82eb519e4e21783bec467
Lyopoly <lydellkid@gmail.com>
2026-07-22 10:11:51 +0800

    chore: configure cloudflare pages preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
+  
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
