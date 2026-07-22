dbc5537be0a775a61a6a128cbe221d2cba21406d
Lyopoly <lydellkid@gmail.com>
2026-07-22 10:49:10 +0800

    chore: configure cloudflare pages preset2

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <UPageHero
-      title="Nuxt Notes"
+      title="LyoNuxt Notes"
       description="A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours."
       :links="[{
         label: 'Get started',
